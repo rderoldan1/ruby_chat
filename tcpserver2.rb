d6751f62c5b248faa8a9148cbb6505323cdbd98f
Ruben Espinosa <rderoldan1@gmail.com>
2011-08-25 19:55:18 -0500

    everybody knows when somebody is online, and the broadcast messages is full

--- tcpserver2.rb
+++ tcpserver2.rb
@@ -1,43 +1,46 @@
 require 'socket'
 
 class Server
+	 server = TCPServer.new(20000)
+        @client = {}
 
-server = TCPServer.new(20000)
-@client = {}
-
- #   def run
 	loop do 
 		Thread.start(server.accept) do |connection|
-#		@client[] << connection	
 			connection.puts ("Welcome, you're connected, please write your name")
 			n = connection.gets
-		        @client [n] = connection
-			puts n 
+		         @client [n] = connection
+				@client.each do |name, sock|
+			                	if  sock.eql? connection
+						puts "is the same user 1"
+						else
+						lin = " #{n} is online!"
+						sock.puts  (lin)
+						end#if	
+				end#each
+						puts n 
 
 				while line = connection.gets
 				break if line =~ /quit/
 				puts line
 				connection.puts ("Received")
 					@client.each do |name, conn|
-			                	if  conn.eql? connection
-						puts "is the same user"
+			                	if  conn.eql? connection 
+						puts "is the same user 2"
 						else
 						puts name, conn
-						conn.puts (line)
-						end					
-				end
+						conn.puts ("#{name} says #{line}")
+						end#if					
+				           end#each
 			
 			end #while
-		
-			connection.write ("Closing the connection")
+			res = @client.find (connection)
+			puts res.name
+			connection.puts ("Closing the connection")
 			connection.close
+
 		end#Thread
 	end#loop
-#   end#run
 
-end
-
-#serv = Server.new()
-#serv.run
+end#server
 
 
